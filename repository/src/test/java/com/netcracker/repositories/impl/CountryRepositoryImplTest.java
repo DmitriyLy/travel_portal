6843a6dcf1df7ab1e4d805f589817777d43856c9
DmitriyLy <dmitriy.lisay@gmail.com>
2016-11-28 15:54:10 +0200

    CountryRepositoryImplTest added

---
+++ repository/src/test/java/com/netcracker/repositories/impl/CountryRepositoryImplTest.java
@@ -0,0 +1,37 @@
+package com.netcracker.repositories.impl;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by dima_2 on 28.11.2016.
+ */
+public class CountryRepositoryImplTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void add() throws Exception {
+
+    }
+
+    @Test
+    public void update() throws Exception {
+
+    }
+
+    @Test
+    public void remove() throws Exception {
+
+    }
+
+    @Test
+    public void query() throws Exception {
+
+    }
+
+}
