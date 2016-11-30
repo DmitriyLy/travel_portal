c61abeeae4135c1cecba53e8e028f1bd3a840faa
DmitriyLy <dmitriy.lisay@gmail.com>
2016-12-01 00:43:54 +0200

    Constructors, getters, setters added into City POJO

--- repository/src/main/java/com/netcracker/entities/City.java
+++ repository/src/main/java/com/netcracker/entities/City.java
@@ -4,4 +4,41 @@ package com.netcracker.entities;
  * Created by dima_2 on 30.11.2016.
  */
 public class City {
+    private int id;
+    private String name;
+    private State state;
+
+    public City() {
+
+    }
+
+    public City(int id, String name, State state) {
+        this.id = id;
+        this.name = name;
+        this.state = state;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
 }
