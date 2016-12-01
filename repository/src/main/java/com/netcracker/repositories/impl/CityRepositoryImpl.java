2dfc9c6151ff1396f1337ccc8293f24cd6754d75
DmitriyLy <dmitriy.lisay@gmail.com>
2016-12-01 23:27:51 +0200

    CityRowMapper mover to com.netcracker.repositories.rowmappers

--- repository/src/main/java/com/netcracker/repositories/impl/CityRepositoryImpl.java
+++ repository/src/main/java/com/netcracker/repositories/impl/CityRepositoryImpl.java
@@ -48,17 +48,4 @@ public class CityRepositoryImpl implements IRepository<City> {
         return null;
     }
 
-    class CityRowMapper implements RowMapper<City> {
-
-        @Override
-        public City mapRow(ResultSet resultSet, int i) throws SQLException {
-
-            City city = new City();
-            city.setId(resultSet.getInt("id"));
-            city.setName(resultSet.getString("name"));
-
-
-            return city;
-        }
-    }
 }
