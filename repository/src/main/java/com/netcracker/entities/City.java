2922ac0f5dbc320c9233b7b9ba9407dea9dc3ab8
DmitriyLy <dmitriy.lisay@gmail.com>
2016-12-01 23:02:46 +0200

    equals, toString, hashCode added to City

--- repository/src/main/java/com/netcracker/entities/City.java
+++ repository/src/main/java/com/netcracker/entities/City.java
@@ -41,4 +41,34 @@ public class City {
     public void setState(State state) {
         this.state = state;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        City city = (City) o;
+
+        if (id != city.id) return false;
+        if (name != null ? !name.equals(city.name) : city.name != null) return false;
+        return state != null ? state.equals(city.state) : city.state == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (state != null ? state.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "City{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", state=" + state +
+                '}';
+    }
 }
