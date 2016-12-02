0a1392fd06bea4f5ede36f82021f4e8f6a781b39
guavaparty <bulgakov113@gmail.com>
2016-12-02 15:29:23 +0200

    POJO Category

--- repository/src/main/java/com/netcracker/entities/Category.java
+++ repository/src/main/java/com/netcracker/entities/Category.java
@@ -4,4 +4,48 @@ package com.netcracker.entities;
  * Created by dima_2 on 30.11.2016.
  */
 public class Category {
+    private long id;
+    private String name;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Category category = (Category) o;
+
+        if (id != category.id) return false;
+        return name != null ? name.equals(category.name) : category.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
