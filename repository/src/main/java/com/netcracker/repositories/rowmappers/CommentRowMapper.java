6567e1111249861afe98933998eb03241eb026a7
Yulia Diskaya <sam_d@ukr.net>
2016-12-02 16:37:12 +0200

    Comments repo, row-mapper

--- repository/src/main/java/com/netcracker/repositories/rowmappers/CommentRowMapper.java
+++ repository/src/main/java/com/netcracker/repositories/rowmappers/CommentRowMapper.java
@@ -1,6 +1,9 @@
 package com.netcracker.repositories.rowmappers;
 
 import com.netcracker.entities.Comment;
+import com.netcracker.repositories.impl.LabelRepositoryImpl;
+import com.netcracker.repositories.impl.UserRepositoryImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.RowMapper;
 
 import java.sql.ResultSet;
@@ -10,8 +13,20 @@ import java.sql.SQLException;
  * Created by Yuksi on 01.12.2016.
  */
 public class CommentRowMapper implements RowMapper<Comment> {
+    @Autowired
+    UserRepositoryImpl userRepository;
+
+    @Autowired
+    LabelRepositoryImpl labelRepository;
+
     @Override
     public Comment mapRow(ResultSet rs, int rowNum) throws SQLException {
-        return null;
+        Comment comment = new Comment();
+        comment.setId(rs.getLong("id"));
+        comment.setUser(userRepository.getById(rs.getLong("user_id")));
+        comment.setLabel(labelRepository.getById(rs.getLong("label_id")));
+        comment.setCommentDate(rs.getDate("comment_date"));
+        comment.setCommentText(rs.getString("comment_text"));
+        return comment;
     }
 }
