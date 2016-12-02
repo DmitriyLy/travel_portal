c51d51917717aa5b1d5f22c80e7a11759f0ae929
DmitriyLy <dmitriy.lisay@gmail.com>
2016-12-02 15:45:28 +0200

    CityRepositoryImpl implemented

--- repository/src/main/java/com/netcracker/repositories/impl/CityRepositoryImpl.java
+++ repository/src/main/java/com/netcracker/repositories/impl/CityRepositoryImpl.java
@@ -1,9 +1,12 @@
 package com.netcracker.repositories.impl;
 
 import com.netcracker.entities.City;
+import com.netcracker.queries.IQueriesRepository;
 import com.netcracker.repositories.IRepository;
 import com.netcracker.repositories.rowmappers.CityRowMapper;
 import com.netcracker.specifications.Specification;
+import com.netcracker.specifications.SqlSpecification;
+import oracle.net.aso.q;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,31 +25,48 @@ public class CityRepositoryImpl implements IRepository<City> {
     private JdbcTemplate template;
 
     @Autowired
-    private CityRowMapper rowMapper;
+    private CityRowMapper cityRowMapper;
 
     @Override
     public void add(City item) {
 
+        String query = IQueriesRepository.INSERT_CITY;
+        int out = template.update(query, new Object[]{item.getState().getId(), item.getName()});
+        if(out == 0) {
+            LOGGER.warn("Cannot insert " + item.toString());
+        }
     }
 
     @Override
     public void update(City item) {
 
+        String query = IQueriesRepository.UPDATE_CITY;
+        int out = template.update(query, new Object[]{item.getState().getId(), item.getName(), item.getId()});
+        if(out == 0) {
+            LOGGER.warn("Cannot insert " + item.toString());
+        }
     }
 
     @Override
     public void remove(City item) {
 
+        String query = IQueriesRepository.DELETE_CITY;
+        int out = template.update(query, new Object[]{item.getId()});
+        if (out == 0) {
+            LOGGER.warn("Cannot delete " + item.toString());
+        }
     }
 
     @Override
     public City getById(long id) {
-        return null;
+        String query = IQueriesRepository.GET_COMMENT_BY_ID;
+        return template.queryForObject(query,new Object[] {id}, cityRowMapper);
     }
 
     @Override
     public List<City> query(Specification specification) {
-        return null;
+        SqlSpecification sqlSpecification = (SqlSpecification) specification;
+        return template.query(sqlSpecification.toSqlQuery(), cityRowMapper);
     }
 
 }
