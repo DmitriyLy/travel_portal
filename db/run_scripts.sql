cd49878f696184504da1357043a275cff5da3593
DmitriyLy <dmitriy.lisay@gmail.com>
2016-12-18 22:02:13 +0200

    Indexes added.

--- db/run_scripts.sql
+++ db/run_scripts.sql
@@ -2,11 +2,13 @@ connect / as sysdba;
 @user_creation.sql
 disconnect;
 connect TRAVEL_PORTAL/TRAVEL_PORTAL
+@drop_indexes.sql
 @drop_tables_sequences.sql
 @create_table_pk.sql
 @create_sequences.sql
 @add_references_fk.sql
 @add_data_constraints.sql
+@create_indexes.sql
 @insert_countries.sql
 @insert_states.sql
 @insert_cities.sql
