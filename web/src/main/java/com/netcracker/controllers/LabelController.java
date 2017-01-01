9903948dcb847e15384fae755e8d033fc20bc031
blaze159 <alex.kozhuchar@gmail.com>
2017-01-01 17:06:25 +0200

    LabelController description blueprint.

--- web/src/main/java/com/netcracker/controllers/LabelController.java
+++ web/src/main/java/com/netcracker/controllers/LabelController.java
@@ -1,17 +1,45 @@
 package com.netcracker.controllers;
 
+import com.netcracker.entities.Attachment;
+import com.netcracker.entities.Comment;
+import com.netcracker.entities.FullLabelInfo;
+
 /**
+ * Controller for label
+ * and related information extraction.
+ * Described in UC4
  *
  * @author Oveian Egor
  * @author Kozhuchar Alexander
  */
-
 public class LabelController {
-
-    public void getFullLabel(){}
-
-    public void getAttachmentByLabelId(){}
-
-    public void getCommentsByLabelId(){}
-
+    /**
+     * Method of full label info extraction
+     * Described in FR13
+     * @param
+     * @return FullLableInfo object
+     */
+    public FullLabelInfo getFullLabel() {
+        return null;
+    }
+    /**
+     * Method of attachment extraction, depending on
+     * which label id came from client
+     * Described in FR14,FR15
+     * @param
+     * @return found attachment by label id
+     */
+    public Attachment getAttachmentByLabelId() {
+        return null;
+    }
+    /**
+     * Method of comment extraction, depending on
+     * which label id came from client
+     * Described in FR16
+     * @param
+     * @return found comment by label id
+     */
+    public Comment getCommentsByLabelId() {
+        return null;
+    }
 }
--- web/src/main/java/com/netcracker/controllers/SearchController.java
+++ web/src/main/java/com/netcracker/controllers/SearchController.java
@@ -17,7 +17,4 @@ public class SearchController {
     public void searchByRating(){}
 
     public void searchByTag(){}
-
-
-
 }
