f6fe7c8502eb14570d4aa672a51ce357d931b18a
blaze159 <alex.kozhuchar@gmail.com>
2017-01-02 15:02:01 +0200

    LabelController was edited

--- web/src/main/java/com/netcracker/controllers/LabelController.java
+++ web/src/main/java/com/netcracker/controllers/LabelController.java
@@ -7,7 +7,8 @@ import com.netcracker.entities.FullLabelInfo;
 /**
  * Controller for label
  * and related information extraction.
- * Described in UC4
+ * Described in :
+ * UC4 - Label view
  *
  * @author Oveian Egor
  * @author Kozhuchar Alexander
@@ -15,31 +16,52 @@ import com.netcracker.entities.FullLabelInfo;
 public class LabelController {
     /**
      * Method of full label info extraction
-     * Described in FR13
-     * @param
+     * by label id
+     * Described in :
+     * FR13 - The system should generate a window containing the following information:
+     * • author's name,
+     * • setup date,
+     * • address,
+     * • rating,
+     * • review
+     * • categories,
+     * • a list of tags separated by commas,
+     * • number of attachments
+     * • number of comments
+     *
+     * @param labelId
      * @return FullLableInfo object
      */
-    public FullLabelInfo getFullLabel() {
+    public FullLabelInfo getFullLabel(long labelId) {
         return null;
     }
+
     /**
      * Method of attachment extraction, depending on
      * which label id came from client
-     * Described in FR14,FR15
-     * @param
+     * Described in:
+     * FR14 - The system should provide the ability to view attachments;
+     * FR15 - The system should provide display of investments in accordance
+     * with the order they are added to the label
+     *
+     * @param labelId
      * @return found attachment by label id
      */
-    public Attachment getAttachmentByLabelId() {
+    public Attachment getAttachmentByLabelId(long labelId) {
         return null;
     }
+
     /**
      * Method of comment extraction, depending on
      * which label id came from client
-     * Described in FR16
-     * @param
+     * Described in:
+     * FR16 - The system should provide the ability to view comments
+     *
+     * @param labelId
      * @return found comment by label id
      */
-    public Comment getCommentsByLabelId() {
+    public Comment getCommentsByLabelId(long labelId) {
         return null;
     }
+
 }
