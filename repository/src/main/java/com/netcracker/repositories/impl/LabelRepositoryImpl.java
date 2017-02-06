11623f27313ef84f548984a1c8c1a84238a1ca51
Yulia Diskaya <sam_d@ukr.net>
2017-02-07 00:58:30 +0200

    deleteLabel

--- repository/src/main/java/com/netcracker/repositories/impl/LabelRepositoryImpl.java
+++ repository/src/main/java/com/netcracker/repositories/impl/LabelRepositoryImpl.java
@@ -100,7 +100,7 @@ public class LabelRepositoryImpl implements LabelRepository {
 
         return item;
     }
-    
+
     @Override
     public void hardRemove(Label item) {
         int out1 = jdbcTemplate.update(QueriesRepository.DELETE_TAGS_LABELS_BY_LABEL, item.getId());
